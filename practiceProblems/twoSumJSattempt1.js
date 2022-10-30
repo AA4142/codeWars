fca987cbb84950333280a44be1e86c73c6e686fb
AA4142 <alexarmatas5@gmail.com>
2022-10-30 17:14:38 -0400

    finished twosum standalone

--- practiceProblems/twoSumJSattempt1.js
+++ practiceProblems/twoSumJSattempt1.js
@@ -4,22 +4,28 @@
  * @return {number[]}
  */
 //coding brute force method
-
 let tar = 9;
 let numbers = [2,15,11,7];
 
 
-
-function twoSum(nums, target) {
+var answer = function twoSum(nums, target){
+    //declaring object to hold our numbers and index
     let numObj = {};
-    for(let i = 0 ; i < nums.length; i++){
-        let complement = target - nums[i];
+
+    //standard for loop to get through every iteration w/o hitting the end end
+    for(i = 0; i < nums.length ; i++){
+        //finding the complement for the specific nums[i]
+        const complement = target - nums[i];
+
+        //checking to see if we have seen the complement
         if(numObj[complement] !== undefined){
-            return [numObj[complement], i];
+            //this returns the index value of the complement and the current index value
+            //of the complemented number
+            return[numObj[[complement]], i];
         }
+        //setting the value so we can compare future complements
         numObj[nums[i]] = i;
-        console.log(numObj)
     }
 }
 
-twoSum(numbers, tar);
+console.log(answer(numbers,tar));

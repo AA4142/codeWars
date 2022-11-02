deb77c6640f8a85ffaa9802ddc13ccee98184492
AA4142 <alexarmatas5@gmail.com>
2022-11-02 04:25:53 -0400

    completed validParentheses by myself

--- practiceProblems/validParentheses.js
+++ practiceProblems/validParentheses.js
@@ -1,55 +1,47 @@
-const testString = "(){}}{";
+const test = "()[]{}"
 
 var isValid = function(s){
-    let parenHolder = "";
-
-
-
-    if(s[0] == ")" || s[0] == "]" || s[0] == "}" || s.length == 1 || s.length % 2 != 0){
-        return false
-    } 
-
-    for (let i = 0; i < s.length; i++){
-        if(s[i] == "(" || s[i] == "[" || s[i] == "{"){
-            parenHolder = parenHolder + s[i];
-        }
-        switch(s[i]){
-            case ")":{
-                if(parenHolder[parenHolder.length-1] == "("){
-                    parenHolder = parenHolder.slice(0, -1);
-                    if(parenHolder.length < 1){
-                        return true;
-                    }
-                }else{
-                    return false;  
-                }
-            }
-            case "]":{
-                if(parenHolder[parenHolder.length-1] == "["){
-                    parenHolder = parenHolder.slice(0, -1);
-                    if(parenHolder.length < 1){
-                        return true;
-                    }
-                }else{
-                    return false;
-                }
-
-            }
-            case "}":{
-                if(parenHolder[parenHolder.length-1] == "{"){
-                    parenHolder = parenHolder.slice(0, -1);
-                    if(parenHolder.length < 1){
-                        return true;
-                    }
+    //let's take care of the edge cases
+    const lastChar = s.charAt(s.length - 1);
+    const firstChar = s.charAt(0);
+    if((s.length % 2 == 1) || lastChar == "(" || lastChar == "{" || lastChar == "[" ||
+        firstChar == ")" || firstChar == "}" || firstChar == "]"){
+            return false;
+    }
+    //now we need to loop through the values
+    let charHolder = "";
+    for(let i = 0; i < s.length; i++){
+        //check for reverse brackets
+        if(s[i] == ")"){
+            if(charHolder.charAt(charHolder.length-1) == "("){
+                charHolder = charHolder.slice(0, -1);
             }else{
                 return false;
             }
 
+        } else if(s[i] == "}"){
+            if(charHolder.charAt(charHolder.length-1) == "{"){
+                charHolder = charHolder.slice(0, -1);
+            }else{
+                return false;
+            }
+
+        } else if(s[i] == "]"){
+            if(charHolder.charAt(charHolder.length-1) == "["){
+                charHolder = charHolder.slice(0, -1);
+            }else{
+                return false;
+            }
+
+        }else{
+            charHolder = charHolder + s[i];
         }
     }
-
-} return false;
-
+    if(charHolder.length == 0){
+        return true
+    }else{
+        return false;
+    }
 }
 
-console.log(isValid(testString));
+console.log(isValid(test));

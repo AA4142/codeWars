605aba836fbcc6d283f6e3e62c925989085f0602
AA4142 <alexarmatas5@gmail.com>
2022-11-10 12:30:16 -0500

    binary search, proper way

--- practiceProblems/Eloquent/practice1.js
+++ practiceProblems/Eloquent/practice1.js
@@ -1,13 +1,21 @@
-const nums = [-1,0,3,5,9,12];
+const nums = [-1,0,3,5,6,7,8,9,12];
 const target = 9
 
-var search = function(nums,target){
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] == target){
-            return i;
+var search = function(nums, target){
+    let left = 0;
+    let right = nums.length - 1;
+    while(left <= right){
+        let mid = Math.floor((right + left) / 2);
+
+        if(nums[mid] === target){
+            return mid;
+        }else if(target < nums[mid]){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
         }
     }
     return -1;
 }
 
-console.log(search(nums, target));
+console.log(search(nums,target));

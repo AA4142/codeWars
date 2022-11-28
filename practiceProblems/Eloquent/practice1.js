9aa602552e9602e3ef7fe45235af36a097a32ffb
AA4142 <alexarmatas5@gmail.com>
2022-11-28 11:27:59 -0500

    inefficient solution to max subarray

--- practiceProblems/Eloquent/practice1.js
+++ practiceProblems/Eloquent/practice1.js
@@ -1,19 +1,79 @@
-/*
- * @param {number[]} nums
- * @return {boolean}
- */
-const nums = [1,2,3,4];
 
- var containsDuplicate = function(nums) {
-    let numsObj = {};
+const nums = [-2,1,-3,4,-1,2,1,-5,4];
 
-    for(let i = 0; i < nums.length; i++){
-        if(numsObj[nums[i]] !== undefined){
-            return true;
-        }else{
-            numsObj[nums[i]] = i;
-        }
+var maxSubArray = function(nums){
+    let globalMaxSum = nums[0];
+    let localMaxSum = nums[0];
+
+    for(let i = 1; i < nums.length; i++){
+        localMaxSum = Math.max(nums[i], localMaxSum + nums[i]);
+        console.log(localMaxSum);
+
+        globalMaxSum = Math.max(localMaxSum, globalMaxSum);
     }
-    return false;
- }
-console.log(containsDuplicate(nums));
+
+    return globalMaxSum;
+
+
+}
+
+console.log(maxSubArray(nums));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var maxSub = function(nums){
+    //declare the local and global max
+    //local max let's us loop
+    //global max will auto return if array val is less than 2;
+    let localMax = nums[0];
+    let globalMax = nums[0];
+
+    for (let i = 1; i < nums.length; i++){
+        localMax = Math.max(localMax, nums[i] + localMax);
+
+        globalMax = Math.max(globalMax, localMax);
+    }
+
+    return globalMax;
+}
+
+
+
+
+
+
+
+
+
+
+
+

94fda9ae46707ec00e5a037c1ec311b8c363b6cb
AA4142 <alexarmatas5@gmail.com>
2022-11-30 01:35:47 -0500

    inefficient solution to ransomnote, next time study efficient solutions for ransomNote

--- practice1.js
+++ practice1.js
@@ -1,23 +1,47 @@
-/*
- * Definition for isBadVersion()
- * 
- * @param {integer} version number
- * @return {boolean} whether the version is bad
- * isBadVersion = function(version) {
- *     ...
- * };
- */
+const ransomNote = "aa";
+const magazine = "aab";
+
+
+ var canConstruct = function(ransomNote, magazine) {
+    let letterHolder = {};
+    for(let i = 0; i < magazine.length; i++){
+        letterHolder[magazine[i]] ? letterHolder[magazine[i]]++ : letterHolder[magazine[i]] = 1;
+    }
+    console.log(letterHolder);
+
+    for(let j = 0 ; j < ransomNote.length; j++){
+        if(!letterHolder[ransomNote[j]]){
+            return false;
+        } else{
+            letterHolder[ransomNote[j]]--
+        }
+    }
+    return true;
+    }
+;
+
+console.log(canConstruct(ransomNote, magazine));
+
+
+
+let string1 = "aaabbcc";
+let ransom = "bacz";
+let emptyObj = {};
+
 
 /*
- * @param {function} isBadVersion()
- * @return {function}
- */
- var solution = function(isBadVersion) {
-    /*
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        
-    };
-};
+for(let i = 0; i < string1.length; i++){
+    if(emptyObj[string1[i]]){
+        emptyObj[string1[i]]++;
+    }else{
+        emptyObj[string1[i]] = 1;
+    }
+}
+
+/*
+for (let j = 0; j < ransom.length; j++){
+    emptyObj[ransom[j]] ? emptyObj[ransom[j]]-- : console.log("false");
+}
+*/
+
+

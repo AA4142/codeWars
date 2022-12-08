7a6a372a532aa571c3ae8892bf67f01318e1461b
AA4142 <alexarmatas5@gmail.com>
2022-12-08 05:48:25 -0500

    when we are testing to see if things are divisble it's easier to see that % == 0;

--- practice1.js
+++ practice1.js
@@ -1,39 +1,16 @@
 
-const string = "ccc";
 
- var longestPalindrome = function(s) {
-    const stringMap = {};
-    let totalLength = 0;
-    let hasOdd = 0;
+let test = 15;
 
-    for(let i = 0; i < s.length; i++){
-        if(stringMap[s[i]]){
-            stringMap[s[i]] += 1;
-        }else{
-            stringMap[s[i]] = 1;
+function solution(number){
+    let sum = 0;
+
+    for(let i = 0; i < number ; i++){
+        if((i % 3 === 0) || (i % 5 === 0) || (i % 3 === 0 && i % 5 === 0)){
+            sum = sum + i;
         }
     }
+    return sum;
+}
 
-    console.log(Object.values(stringMap).length);
-    console.log(Object.values(stringMap));
-
-    if(Object.values(stringMap).length == 1){
-        let tempVal = Object.values(stringMap);
-        console.log(tempVal);
-    }
-
-
-    Object.values(stringMap).forEach((val) => {
-        if(val >= 2){
-            let evenValues = Math.floor(val / 2);
-            totalLength = totalLength + (evenValues * 2);
-        }else{
-            hasOdd = 1;
-        }
-    });
-
-    return(totalLength + hasOdd);
-    
-};
-
-longestPalindrome(string);
+console.log(solution(test));
